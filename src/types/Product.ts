3caa619df73e286fb6176a432e9ba8c34aa88316
Lazar Muresan <lazarmuresan@gmail.com>
2023-05-23 13:16:58 +0300

    update type

--- src/types/Product.ts
+++ src/types/Product.ts
@@ -4,13 +4,3 @@ export interface Product {
   price: number;
   imageUrl: string;
 }
-
-// type Inventory = { [k: string]: number };
-
-// enum Category {
-//   SKI = "ski",
-//   SNOWBOARD = "Snowboard",
-//   SKATE = "Skate",
-//   SURF = "Surf",
-//   SKATEBOARD = "Skateboard",
-// }
